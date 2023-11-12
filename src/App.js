4f2334bf5951d39c5991614a4abc5908711d6074
Harshal Dhote <dhoteh020@gmail.com>
2023-11-12 12:26:17 +0530

    new one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import React,{ useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+//run code based on the condition
+useEffect(()=>{
+  const token= getTokenFromUrl(); //getting tokenn from url :)
+  console.log("Got the token >>>  ",token);
+
+
+},[]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+   
+
+      <Login />
+
     </div>
   );
 }
