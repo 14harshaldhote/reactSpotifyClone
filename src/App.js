7536e22a44c6304cd8c97a3efdab780fa81470c0
Harshal Dhote <dhoteh020@gmail.com>
2023-11-12 13:05:19 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 
-import React,{ useEffect } from 'react';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify= new SpotifyWebApi();
+
+
 
 function App() {
+  const[token,setToken] =useState(null);  
 //run code based on the condition
 useEffect(()=>{
-  const token= getTokenFromUrl(); //getting tokenn from url :)
-  console.log("Got the token >>>  ",token);
+  const hash= getTokenFromUrl(); //getting tokenn from url :)
+  window.location.hash = ""; // it will show clean url
+  const _token=hash.access_token;
 
+    if(_token){
+      setToken(_token);
 
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log("👨 ", user);
+      })
+    }
+
+  console.log("Got the token 👉  ",token);
 },[]);
 
-
-
   return (
-    <div className="app">
-   
-
-      <Login />
-
+    <div className="app">{ token ? ( <Player /> ):( <Login /> )}
     </div>
   );
 }
